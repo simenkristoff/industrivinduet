c8552dcb90ee7ea0ca5d7916c9189f907a3317dd
Simen Kristoffersen <simekri@stud.ntnu.no>
2021-02-16 20:30:54 +0100

    Tester for crud containers

---
+++ client/containers/crud/__tests__/UserContainer.spec.tsx
@@ -0,0 +1,56 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import * as reactRedux from 'react-redux';
+
+import { UserContainer } from '@/containers/crud/UserContainer';
+import { CrudContainer } from '@/containers/crud/CrudContainer';
+import configureStore from '@/state/configureStore.dev';
+import { UserActionTypes, MemberActionTypes } from '@/state/interface';
+
+const initialState = (window as any).initialReduxState;
+const spy = jest.spyOn(reactRedux, 'useDispatch');
+
+const { Provider } = reactRedux;
+describe('container <UserContainer />', () => {
+  let wrapper: any, store: any, container: any, component: any;
+  beforeEach(() => {
+    store = configureStore(initialState);
+    store.dispatch = jest.fn();
+    wrapper = mount(
+      <Provider store={store}>
+        <UserContainer />
+      </Provider>,
+    );
+    container = wrapper.find(UserContainer);
+    component = container.find(CrudContainer);
+    wrapper.unmount();
+  });
+
+  it('should render both container and component', () => {
+    expect(container.length).toBeTruthy();
+    expect(component.length).toBeTruthy();
+  });
+
+  it('should map state to props', () => {
+    const expectedPropKeys = ['state', 'columns', 'name', 'collection', 'dataForm'];
+
+    expect(Object.keys(component.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+  });
+
+  it('should map dispatch to props', () => {
+    const expectedPropKeys = ['fetch', 'create', 'update', 'remove', 'set'];
+
+    expect(Object.keys(component.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+  });
+
+  it('should fetch data on mount', () => {
+    const actions: Array<any> = store.dispatch.mock.calls;
+    expect(actions.length).toBeGreaterThan(0);
+    expect(
+      actions.some((action) => action[0]['type'] === UserActionTypes.FETCH.START),
+    ).toBeTruthy();
+    expect(
+      actions.some((action) => action[0]['type'] === MemberActionTypes.FETCH.START),
+    ).toBeTruthy();
+  });
+});
